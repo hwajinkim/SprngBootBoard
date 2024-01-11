bc2cca512007c5bcfdd08c71e2ab8e91f7393b9d
hwajinkim <hwajin0720@naver.com>
2024-01-11 17:16:55 +0900

    게시물 리스트 생성, 게시물 삭제 기능 완성

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,9 +4,12 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class BoardController {
@@ -23,5 +26,23 @@ public class BoardController {
     public String boardWritePro(Board board){
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardview(Model model, Integer id){
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
     }
 }
